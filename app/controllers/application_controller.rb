b10f9c3c86070a4b3e541b33f795806f879deedd
Garbage-soft <wake.awakens3@gmail.com>
2022-07-05 21:26:08 +0900

    追加:トップページにCSS適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
+
+  private def set_layout
+    if params[:controller].match(%r{\A(store|admin)/})
+      Regexp.last_match[1]
+    else
+      'store'
+    end
+  end
 end
